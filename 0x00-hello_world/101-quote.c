0caf649c772b0a0a334ed8575b7b8bb7086d418a
Khalid Magdy <km373413@gmai,com>
2023-07-13 19:09:24 -0700

    6-size.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - A program that prints line without printf or put
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
