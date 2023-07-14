523b03c411a33ea5ea9f8f19ffce836e00845423
Khalid Magdy <km373413@gmai,com>
2023-07-13 18:47:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,11 @@
-#include <stidio.h>
+#include <stdio.h>
 /**
- * main - Description 
+ * main - A program that prints line with printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("proper grammar, but the outcome is a piece of art,/n");
+return (0);
+}
+
