fb309574d0cc04b05828d6ca30510aa2386119c0
Khalid Magdy <km373413@gmai,com>
2023-07-14 19:09:58 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints integers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+for (a = '0' ; a < '10' ; ++a)
+{
+putchar(a + '0')
+}
+putchar('\n');
+return (0);
+}
