b2043ff3c337d90a84afabb5a89d42a44266ec49
Khalid Magdy <km373413@gmai,com>
2023-07-18 16:45:27 -0700

    print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - A program that check alphabet in lower case
+ *
+ * @n : intiger character in ASCII
+ *
+ * Return: 1 if lower , 0 if not  (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('43');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (0);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
