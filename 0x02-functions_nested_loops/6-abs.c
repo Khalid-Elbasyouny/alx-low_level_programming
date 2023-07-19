fafac7b9546039d35d638459eef2cd53fa3f7db6
Khalid Magdy <km373413@gmai,com>
2023-07-18 17:36:37 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs- A program that computes the absolute value of an integer.
+ *
+ * @s : intiger character
+ *
+ * Return: intiger for the absoulute value
+ */
+int _abs(s)
+{
+	if (s >= 0)
+	{
+		return (s);
+	}
+	else
+	{
+	return (s * -1);
+	}
+	_putchar('\n');
+}
