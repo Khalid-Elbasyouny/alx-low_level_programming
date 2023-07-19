cf4eee42359361737b388521061f5389ed478aa1
Khalid Magdy <km373413@gmai,com>
2023-07-18 19:20:57 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - A program that prints alphabet in lower case
+ *
+ * @c : intiger character in ASCII
+ *
+ * Return: 1 if lower , 0 if not  (Success)
+ */
+void times_table(void)
+{       
+        
+} 
