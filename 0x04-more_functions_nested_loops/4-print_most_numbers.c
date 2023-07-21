610f9cb5b9a8769a23619169abb646d009935d6d
Khalid Magdy <km373413@gmai,com>
2023-07-20 17:34:46 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: 0 ;
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(i + 48);
+		}
+	}
+	_putchar('\n');
+}
