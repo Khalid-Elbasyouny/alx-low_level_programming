a713b84ba817060984f01e1ee15fa31d353c2d6e
Khalid Magdy <km373413@gmai,com>
2023-07-20 17:47:54 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: 0 ;
+ */
+
+void more_numbers(void);
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			_putchar(x + 48);
+		}
+	}
+	_putchar('\n');
+}
