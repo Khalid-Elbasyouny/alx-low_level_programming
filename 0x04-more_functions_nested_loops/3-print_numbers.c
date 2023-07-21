b717f13ed36f88fd246fb23e7cc99e3b736f4d29
Khalid Magdy <km373413@gmai,com>
2023-07-20 17:09:30 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: 0 ;
+ */
+
+void print_numbers(void)
+{
+	int i
+	for (i = 0; i < 10; i++)
+	{
+		_putchar("%d", i);
+	}
+	_putchar("\n");
+}
