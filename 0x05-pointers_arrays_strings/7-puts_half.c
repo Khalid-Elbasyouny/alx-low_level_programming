6333e9b6fc54131c7994525d75a29910e092843d
Khalid Magdy <km373413@gmai,com>
2023-07-24 19:34:40 -0700

    6-put long

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half -  a function that prints n
+ * elements of an array of integers,
+ *
+ * @str: string parameter
+ * Return: void
+*/
+
+void puts_half(char *str);
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
