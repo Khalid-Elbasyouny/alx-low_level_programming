f65f88bd26865376974060c2544c5e668a650683
Khalid Magdy <km373413@gmai,com>
2023-08-04 18:42:51 -0700

    argv ,argc task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - a program that print his name
+ *
+ * @argv: array .
+ * @argc: int 'counter'
+ *
+ * Return: 0
+ *
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n" argv[0]);
+	return (0);
+}
