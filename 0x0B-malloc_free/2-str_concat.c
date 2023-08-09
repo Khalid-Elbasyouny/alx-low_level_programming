5f522996010501791e1e48692b3079a75f1e0daa
Khalid Magdy <km373413@gmai,com>
2023-08-08 17:37:51 -0700

    task 3 memo

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,17 +1,59 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 
 /**
- * * str_concat - function that concatenates two strings.
+ * _strlen - function that find the string length
+ *
+ * @s: a given string
+ *
+ * Return: int for the length
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
+
+/**
+ * *str_concat - function that concatenates two strings.
  *
  * @s1: first strings
  * @s2: second strings
  *
- * Return: 0 (sucesss)
+ * Return: pointer (sucesss)
  *
 */
 
 char *str_concat(char *s1, char *s2)
-{}
+{
+	int size_s1, size_s2, i;
+	char *n;
+
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+
+	size_s1 = _strlen(s1);
+	size_s2 = _strlen(s2);
+	n = malloc((size_s1 + size_s2) * sizeof(char) + 1);
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i <= size_s1 + size_s2 ; i++)
+	{
+		if (i < size_s1)
+		{
+			n[i] = s1[i];
+		}
+		else
+		{
+			n[i] = s2[i - size_s1];
+		}
+	}
+	n[i] = '\0';
+	return (n);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * **alloc_grid - function that returns a pointer
+ * to a 2 dimensional array of integers.
+ *
+ * @width: width
+ * @height: height
+ *
+ * Return: 0 (sucesss)
+ *
+*/
+int **alloc_grid(int width, int height)
+{}
