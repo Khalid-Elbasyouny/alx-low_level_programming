5e6098c4cdcca16edd470e005e61947ae06c5d61
Khalid Magdy <km373413@gmai,com>
2023-08-08 18:02:13 -0700

    task 3 memo

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,4 +14,29 @@
  *
 */
 int **alloc_grid(int width, int height)
-{}
+{
+	int **mtd, i, j;
+
+	mtd = malloc(sizeof(*mtd) * height);
+	if (width <= 0 || height <= 0 || mtd == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			mtd[i] = malloc(sizeof(**mtd) * width);
+			if (mtd[i] == 0)
+			{
+				while (i--)
+					free(mtd[i]);
+				free(mtd);
+				return (0);
+			}
+			for ( j = 0; j < width; j++)
+				mtd[i][j] = 0;
+		}
+	}
+	return (mtd);
+}
