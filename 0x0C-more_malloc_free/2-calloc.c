c88b0064a553a6b9195540d8f43fb1aff517d09a
Khalid Magdy <km373413@gmai,com>
2023-08-11 15:16:29 -0700

    task submition

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  *_memoset - a function that fill the memory
+ *
+ * @s: pointer
+ * @c: the char to set
+ * @n: maxmun amount of bytes
+ *
+ * Return: pointer  (sucesss)
+ *
+*/
+
+char *_memoset(char *s, char c, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = c;
+	return (ptr);
+}
+
+/**
+ * _calloc - a function that  allocates memory for an array
+ *
+ * @nmemb: a
+ * @size: a
+ * Return: pointer  (sucesss)
+ *
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+	return (m);
+}
