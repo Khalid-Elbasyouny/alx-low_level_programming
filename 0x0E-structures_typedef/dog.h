e0d125d6d0f16950b9fced5b81e70298cb5383b6
Khalid Magdy <km373413@gmai,com>
2023-08-14 18:47:11 -0700

    first task commition

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef dog_H
-#define dog_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - a dog strrct
@@ -16,4 +16,4 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif /*dog_H*/
+#endif /*DOG_H*/
