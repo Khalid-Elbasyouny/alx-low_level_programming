7b43787e68008f482a2d268161d5a317a004ea41
Khalid Magdy <km373413@gmai,com>
2023-08-14 18:40:55 -0700

    first task commition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+ * description: long dof struct 
+ * struct dog _a struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner:  the owner's name
+ *
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*dog_H*/
