cb96258d28e5039aeac303989cf2f352ca15b478
Khalid Magdy <km373413@gmai,com>
2023-08-17 08:35:39 -0700

    commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name. 
+ * print_name - function that prints a name.
  *@f: pointer to print function
  *@name: string name
- * 
+ *
  * Return:void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name  && f)
+	if (name && f)
 		f(name);
 }
