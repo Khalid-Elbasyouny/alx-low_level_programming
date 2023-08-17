7bcee3bc7996414fd00caa4cdc6080abccbac860
Khalid Magdy <km373413@gmai,com>
2023-08-17 13:48:10 -0700

    task commition

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op-add - adding
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op-sub - supstraction
+ * @a: first int
+ * @b: second int
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multipling
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op-div - divition
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return ( a / b);
+}
+
+/**
+ * op-mod - remainder 
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return ( a % b);
+}
