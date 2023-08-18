60e16515896c0b22d1d277239f5ff4868c5a264e
Khalid Magdy <km373413@gmai,com>
2023-08-17 18:33:53 -0700

    task commition

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - a function that
+ *
+ * @n:
+ * @*separator:
+ * Return:
+ *
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - a function that
+ *
+ * @format:
+ * Return:
+ *
+*/
+void print_all(const char * const format, ...)
+{
+}
