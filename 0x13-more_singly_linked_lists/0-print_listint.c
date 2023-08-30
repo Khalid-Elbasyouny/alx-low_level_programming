f6ff2902eff7e60a8896f15bf8f3f4bb9bdcc311
Khalid Magdy <km373413@gmai,com>
2023-08-29 18:09:00 -0700

    tasks

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include “lists.h”
+
+/**
+ * print_listint -  function that prints all the elements
+ * @h: pointer to first node
+ * Return:list size
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
